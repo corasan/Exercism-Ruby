3287a4875bfafa1a67d5ebd09ce12bfb9e57a9be
henrypl95 <henrypl360@gmail.com>
2015-09-30 15:47:20 -0400

    added a more meaningful error message

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,8 +2,7 @@ require 'pry'
 VERSION = 1
 class Hamming
     def self.compute(a, b)
-        # code is cleaner
-        raise(ArgumentError) unless a.length == b.length
+        raise ArgumentError, "Distance between both strands are different." unless a.length == b.length
         a.chars.zip(b.chars).count{|x,y| x != y}
     end
 end
