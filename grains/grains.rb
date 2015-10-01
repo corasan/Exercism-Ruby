8e8caeb6273cf14cd348bd30d6f6b8a11dbd29ef
henrypl95 <henrypl360@gmail.com>
2015-10-01 17:47:48 -0400

    solution is way simpler

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,16 +1,9 @@
 class Grains
   def self.square(n)
-    @new_arr = [1]
-    squares = 1.upto(64).to_a
-    squares.inject do |a, b|
-      result = a * 2
-      @new_arr << result
-      b = result
-    end
-    @new_arr[n-1]
+    2 ** (n - 1)
   end
 
   def self.total
-    @new_arr.inject {|a, b| a + b}
+    2 ** 64 - 1
   end
 end
