fcd7fd473b896b3c75f0680ffb5ac2c080a1d716
henrypl95 <henrypl360@gmail.com>
2015-09-30 20:08:11 -0400

    changed

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -2,11 +2,11 @@ VERSION = 1
 class Phrase
   def initialize(phrase)
     @words = phrase.downcase.gsub(/((?:\s)\W)|((?:\W)\s)|((?:\W)\W)|(\,)|(\.)/, " ").split(" ")
+    @words_hash = {}
   end
 
   def word_count
-    words_hash = {}
     @words.each { |w| words_hash[w] = @words.count(w) }
-    words_hash
+    @words_hash
   end
 end
